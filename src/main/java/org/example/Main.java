a93547090154e7ffe25fa20b6e8588eccf0b3f68
Giandonn <lucas_raineri04@hotmail.com>
2024-10-25 19:11:04 -0300

    configs basicas do projeto

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,22 @@
+package org.example;
+
+/**
+ * Esta é a classe principal do projeto que executa o programa.
+ */
+public class Main {
+
+    // Verificar se esta correto fazer dessa forma
+
+    /**
+     * Método principal que inicia a execução do programa.
+     *
+     * @param args argumentos da linha de comando
+     */
+    public static void main(String[] args) {
+        System.out.printf("hello mundo");
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
